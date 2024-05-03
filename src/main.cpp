a34b0b02bd77d72ed695ea5441d4b55a4b3fe6fa
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-05-03 12:50:31 +0200

    Simple textbox application created

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <QApplication>
+#include <QLineEdit>
 #include <QLabel>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QPushButton>
+#include <QMessageBox>
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
+    QWidget window;
+    QVBoxLayout layout(&window);
+
     QLabel label("Toby application");
-    label.show();
+    layout.addWidget(&label);
+
+    QLineEdit textbox;
+    textbox.setText("Hello World");
+    layout.addWidget(&textbox);
+
+    QPushButton button("Click me");
+    layout.addWidget(&button);
+
+    QObject::connect(&button, &QPushButton::clicked, [&]() {
+        QMessageBox::information(&window, "Information", textbox.text());
+    });
+
+    window.show();
     return app.exec();
 }
