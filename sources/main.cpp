3a1c378a9f673cfd55ab29ba34619f44f20b9f1b
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-05-28 17:00:23 +0200

    Backup

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,6 +5,38 @@
 #include "pcinfo.h"
 #include "logic.h"
 #include "sensor.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using sensor::Sensor;
+using sensor::SensorReply;
+using sensor::SensorRequest;
+
+class SensorClient {
+ public:
+  SensorClient(std::shared_ptr<Channel> channel)
+      : stub_(Sensor::NewStub(channel)) {}
+
+  std::string CallSensor(const std::string& command) {
+    SensorRequest request;
+    request.set_command(command);
+    SensorReply reply;
+    ClientContext context;
+    Status status = stub_->callSensor(&context, request, &reply);
+    if (status.ok()) {
+      //return reply.latitude();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
+    }
+  }
+
+ private:
+  std::unique_ptr<Sensor::Stub> stub_;
+};
 
 int main(int argc, char *argv[])
 {
